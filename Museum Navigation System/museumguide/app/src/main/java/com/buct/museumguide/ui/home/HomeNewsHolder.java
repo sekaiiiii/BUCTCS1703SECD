8b11984f35673e8b23e1824d3012b3b23957919b
HYX <1026411355@qq.com>
2020-04-29 23:33:57 +0800

    首页增加轮播图

---
+++ Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/home/HomeNewsHolder.java
@@ -0,0 +1,32 @@
+package com.buct.museumguide.ui.home;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.buct.museumguide.R;
+
+public class HomeNewsHolder extends RecyclerView.ViewHolder {
+    public Context context;
+    public TextView newsType;
+    public TextView newsTitle;
+    public TextView newsContent;
+    public TextView newsTime;
+    public ImageView newsImg;
+
+    public HomeNewsHolder(Context context, @NonNull View view) {
+        super(view);
+        this.newsType = (TextView) view.findViewById(R.id.newsType);
+        Drawable drawableTop = context.getResources().getDrawable(R.drawable.ic_homenews);
+        this.newsType.setCompoundDrawablesWithIntrinsicBounds(null, drawableTop, null,null);
+        this.newsTitle = (TextView) view.findViewById(R.id.homeNewsTitle);
+        this.newsContent = (TextView) view.findViewById(R.id.homeNewsContent);
+        this.newsTime = (TextView) view.findViewById(R.id.homeNewsTime);
+        this.newsImg = (ImageView) view.findViewById(R.id.homeNewsImg);
+    }
+}
