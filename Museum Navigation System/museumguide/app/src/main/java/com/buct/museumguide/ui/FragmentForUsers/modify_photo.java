cdcfcd8e9450cfe43944c78949b23549259f4f61
tzx666 <916196773@qq.com>
2020-04-30 14:17:19 +0800

    完善页面

---
+++ Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/FragmentForUsers/modify_photo.java
@@ -0,0 +1,38 @@
+package com.buct.museumguide.ui.FragmentForUsers;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.buct.museumguide.R;
+
+public class modify_photo extends Fragment {
+
+    private ModifyPhotoViewModel mViewModel;
+
+    public static modify_photo newInstance() {
+        return new modify_photo();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.modify_photo_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ModifyPhotoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
