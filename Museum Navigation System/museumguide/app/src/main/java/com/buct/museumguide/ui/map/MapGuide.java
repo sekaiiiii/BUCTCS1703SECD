c026109dfc648cedc4d4c6122c0131ca565bda37
tzx666 <916196773@qq.com>
2020-05-02 22:45:53 +0800

    更新json解析办法

--- Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/map/MapGuide.java
+++ Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/map/MapGuide.java
@@ -1,8 +1,14 @@
 package com.buct.museumguide.ui.map;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
@@ -10,13 +16,24 @@ import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.buct.museumguide.R;
+import com.buct.museumguide.util.WebHelper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /*
 * 好像fragment比较麻烦，就直接activity了
 * */
 public class MapGuide extends AppCompatActivity {
+    private static final String TAG ="map" ;
     MapView mMapView = null;
     ArrayList<MuseumMapInfo>mapinfo=new ArrayList<>();
     ArrayList<Marker>markers=new ArrayList<>();
@@ -25,6 +42,24 @@ public class MapGuide extends AppCompatActivity {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_guide);
+        OkHttpClient client= WebHelper.getInstance().client;
+        Request request=new Request.Builder().url("http://192.144.239.176:8080/api/android/get_position")
+                .addHeader("Content-Type", "application/json; charset=utf-8").build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e(TAG, "onFailure: ",e );
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                String res=response.body().string();
+                System.out.println(res.toString());
+                Gson gson = new Gson();
+                mapinfomation map=gson.fromJson(res,mapinfomation.class);
+                System.out.println(map.getDatas().getList().size());
+            }
+        });
         //这里应使用http请求的结果来调用
         mapinfo.add(new MuseumMapInfo(39.906901,116.397972));
         mapinfo.add(new MuseumMapInfo(39.406901,116.297972));
