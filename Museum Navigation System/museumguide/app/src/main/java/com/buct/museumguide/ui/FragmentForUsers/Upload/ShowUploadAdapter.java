6986e469ef1ab24c15a288e400549c1ac223c5fd
tzx666 <916196773@qq.com>
2020-05-14 16:42:37 +0800

    添加播放动画

--- Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/FragmentForUsers/Upload/ShowUploadAdapter.java
+++ Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/FragmentForUsers/Upload/ShowUploadAdapter.java
@@ -1,12 +1,17 @@
 package com.buct.museumguide.ui.FragmentForUsers.Upload;
 
+import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.buct.museumguide.R;
+import com.race604.drawable.wave.WaveDrawable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -15,17 +20,25 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ShowUploadAdapter extends RecyclerView.Adapter<ShowUploadAdapter.ViewHolder> {
     private List<audioitem> list;
     public MyClick click;
-
+    private Context context;
+    private int count;
+    private int length;
+    private WaveDrawable mWaveDrawable;
+    public List<Boolean>isture;
+    private final int MAX_LEN=10000;
+    private final int MID_LEN=5000;
     static class ViewHolder extends RecyclerView.ViewHolder{
         TextView title;
         TextView name;
         TextView author;
+        ImageView imageView;
 
         public ViewHolder (View view)
         {    super(view);
             title=view.findViewById(R.id.textView16);
             name=view.findViewById(R.id.textView17);
             author=view.findViewById(R.id.textView18);
+            imageView=view.findViewById(R.id.imageView);
         }
 
     }
@@ -36,7 +49,16 @@ public class ShowUploadAdapter extends RecyclerView.Adapter<ShowUploadAdapter.Vi
     public interface MyClick{
         void click(View v);
     }
-    public ShowUploadAdapter(List<audioitem> l){this.list=l;}
+    public ShowUploadAdapter(List<audioitem> l,Context context){
+        this.list=l;
+        System.out.println("初始化0"+l.size());
+        this.isture=new ArrayList<>();
+        for(int i=0;i<10000;i++)isture.add(true);
+        System.out.println("初始化"+isture.size());
+        this.context=context;
+        count=0;
+        this.length=10000;
+    }
     @NonNull
     @Override
     public ShowUploadAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -46,7 +68,13 @@ public class ShowUploadAdapter extends RecyclerView.Adapter<ShowUploadAdapter.Vi
             @Override
             public void onClick(View v) {
                 if(click != null){
+                    count++;
                     click.click(v);
+                    if(count%2==1)
+                    length=5000;
+                    else
+                        length=10000;
+                    notifyDataSetChanged();
                 }
             }
         });
@@ -59,6 +87,13 @@ public class ShowUploadAdapter extends RecyclerView.Adapter<ShowUploadAdapter.Vi
             holder.title.setText(item.getTitle());
             holder.name.setText(item.getFilename());
             holder.author.setText(item.getAuthor());
+         mWaveDrawable = new WaveDrawable(context,R.color.colorBrown);
+        holder.imageView.setImageDrawable(mWaveDrawable);
+        /**/if(isture.get(position)==true)
+            mWaveDrawable.setLevel(10000);
+        else
+            mWaveDrawable.setLevel(5000);
+        System.out.println(isture.size());
     }
     @Override
     public int getItemCount() {
