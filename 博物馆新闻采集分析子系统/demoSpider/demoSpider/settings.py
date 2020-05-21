bfdf385e6a78b280b1171af3ed95919c3a191edd
Ice-Jeffrey <735429132@qq.com>
2020-05-21 09:40:40 +0800

    修改了爬虫项目的某些配置文件

--- 博物馆新闻采集分析子系统/demoSpider/demoSpider/settings.py
+++ 博物馆新闻采集分析子系统/demoSpider/demoSpider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'demoSpider.spiders'
 #USER_AGENT = 'demoSpider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'demoSpider.middlewares.DemospiderDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'demoSpider.middlewares.DemospiderDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
