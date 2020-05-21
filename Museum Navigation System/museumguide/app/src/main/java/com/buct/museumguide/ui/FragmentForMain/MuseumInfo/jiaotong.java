cb8a0b2c8dbfb1435574ebc2840b4867d3590ec0
tzx666 <916196773@qq.com>
2020-05-21 22:13:38 +0800

    添加地图

--- Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/FragmentForMain/MuseumInfo/jiaotong.java
+++ Museum Navigation System/museumguide/app/src/main/java/com/buct/museumguide/ui/FragmentForMain/MuseumInfo/jiaotong.java
@@ -20,8 +20,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.TextureMapView;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.buct.museumguide.R;
 
 import java.util.ArrayList;
@@ -29,7 +36,8 @@ import java.util.List;
 
 //拼音的这几个用于给viewpaper使用，直接切换
 public class jiaotong extends Fragment {
-
+    private TextureMapView textureMapView;
+    private AMap aMap;
     private MuseumInfoViewModel mViewModel;
     SharedPreferences infos;
     private boolean isAvilible(Context context, String packageName){
@@ -65,17 +73,29 @@ public class jiaotong extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        textureMapView = (TextureMapView) getView().findViewById(R.id.map);
+
+        if (textureMapView != null) {
+            textureMapView.onCreate(savedInstanceState);
+            aMap = textureMapView.getMap();
+        }
         mViewModel = new ViewModelProvider(this).get(MuseumInfoViewModel.class);
+        TextView textView=getActivity().findViewById(R.id.textView4);
         // TODO: Use the ViewModel
         infos=getActivity().getSharedPreferences("data",Context.MODE_PRIVATE);
+        String lat=infos.getString("Latitude","0");
+        String log=infos.getString("Longtitude","0");
+        String msname=infos.getString("info","中国地质博物馆");
+        System.out.println("博物馆地图的名字"+msname);
+        LatLng latLng = new LatLng(Double.valueOf(lat),Double.valueOf(log));
+        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title(msname));
+        aMap. moveCamera(CameraUpdateFactory.changeLatLng(latLng));
+        textView.setText(msname);
         Button button=getView().findViewById(R.id.button9);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(isAvilible(getActivity(),"com.autonavi.minimap")){
-                    String lat=infos.getString("Latitude","0");
-                    String log=infos.getString("Longtitude","0");
-                    String msname=infos.getString("info","");
                     Log.d("pos123", "onClick: "+lat+" "+log+" "+msname);
                     Intent intent = new Intent();
                     intent.setAction(Intent.ACTION_VIEW);
@@ -92,4 +112,32 @@ public class jiaotong extends Fragment {
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        textureMapView.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        textureMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        textureMapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        textureMapView.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
